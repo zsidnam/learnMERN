f5ddc7c7f8c92aabbcebdbbf95032af750746106
Zachary Sidnam <zac@sidnam.com>
2019-03-17 15:51:04 -0700

    Implemented login and logout

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from '../../actions/authActions';
 
 class Login extends Component {
 	constructor() {
@@ -9,6 +13,22 @@ class Login extends Component {
 			password: '',
 			errors: {}
 		};
+	}
+
+	componentDidMount() {
+		if (this.props.auth.isAuthenticated) {
+			this.props.history.push('/dashboard');
+		}
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.auth.isAuthenticated) {
+			this.props.history.push('/dashboard');
+		}
+
+		if (nextProps.errors) {
+			this.setState({ errors: nextProps.errors });
+		}
 	}
 
 	onChange = e => {
@@ -23,10 +43,12 @@ class Login extends Component {
 			password: this.state.password
 		};
 
-		console.log(user);
+		this.props.loginUser(user);
 	};
 
 	render() {
+		const { errors } = this.state;
+
 		return (
 			<div className='login'>
 				<div className='container'>
@@ -36,26 +58,36 @@ class Login extends Component {
 							<p className='lead text-center'>
 								Sign in to your DevConnector account
 							</p>
-							<form onSubmit={this.onSubmit}>
+							<form noValidate onSubmit={this.onSubmit}>
 								<div className='form-group'>
 									<input
 										type='email'
-										className='form-control form-control-lg'
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.email
+										})}
 										placeholder='Email Address'
 										name='email'
 										value={this.state.email}
 										onChange={this.onChange}
 									/>
+									{errors.email && (
+										<div className='invalid-feedback'>{errors.email}</div>
+									)}
 								</div>
 								<div className='form-group'>
 									<input
 										type='password'
-										className='form-control form-control-lg'
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password
+										})}
 										placeholder='Password'
 										name='password'
 										value={this.state.password}
 										onChange={this.onChange}
 									/>
+									{errors.password && (
+										<div className='invalid-feedback'>{errors.password}</div>
+									)}
 								</div>
 								<input type='submit' className='btn btn-info btn-block mt-4' />
 							</form>
@@ -67,4 +99,18 @@ class Login extends Component {
 	}
 }
 
-export default Login;
+Login.propTypes = {
+	loginUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth,
+	errors: state.errors
+});
+
+export default connect(
+	mapStateToProps,
+	{ loginUser }
+)(Login);
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Landing extends Component {
+	componentDidMount() {
+		if (this.props.auth.isAuthenticated) {
+			this.props.history.push('/dashboard');
+		}
+	}
+
 	render() {
 		return (
 			<div className='landing'>
@@ -16,10 +24,10 @@ class Landing extends Component {
 									from other developers
 								</p>
 								<hr />
-								<Link to={'/login'} className={'btn btn-lg btn-info mr-2'}>
+								<Link to={'/register'} className={'btn btn-lg btn-info mr-2'}>
 									Sign Up
 								</Link>
-								<Link to={'/register'} className={'btn btn-lg btn-light'}>
+								<Link to={'/login'} className={'btn btn-lg btn-light'}>
 									Login
 								</Link>
 							</div>
@@ -31,4 +39,12 @@ class Landing extends Component {
 	}
 }
 
-export default Landing;
+Landing.propTypes = {
+	auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth
+});
+
+export default connect(mapStateToProps)(Landing);
